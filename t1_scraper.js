1d7e06fb9a2f1c27614de3d6705fd4b816e9d88c
daniel-k-richardson <danielr9@utas.edu.au>
2017-02-19 20:46:56 +1100

    first commit

--- t1_scraper.js
+++ t1_scraper.js
@@ -9,7 +9,6 @@ function (context, args) {
   function search (regex, response) {
     let matches = regex.exec(response)
     let resultsOfMatch = []
-    let i = 0
 
     // regex only returns a single match unless you keep calling it. the while
     // loop is to keep calling until the end of the match is found. All these
@@ -19,10 +18,12 @@ function (context, args) {
       if (matches.index === regex.lastIndex) {
         regex.lastIndex++
       }
-
+      // it just so happens a single name can be mentioned twice in both regex
+      // mattern matching, therefore the or works ok (but it isn't perfect).
       resultsOfMatch.push(matches[1] || matches[2])
       matches = regex.exec(response)
     }
+    // an array based on regex match patterns
     return [...new Set(resultsOfMatch)]
   }
 
@@ -37,15 +38,12 @@ function (context, args) {
     response = stringFormat(args.target.call({}))
     let siteNav = search(/\s([A-Za-z_]+):/g, response)
 
-    // name of nav + name of page {nav:"info"}
+    // call scripter with nav + site page, i.e. {nav:"info"}
     page[siteNav] = sitePages[0]
 
-    // get the wall of text from page name
+    // get the wall of text from page name and extract all usernames.
     response = stringFormat(args.target.call(page))
-
-    // -- troy_cole
-    // madthugpug of project
-    let usernames = search(/([A-Za-z0-9_-]+)\s\of\s\project|-{2}\s([A-Za-z_]+)\s/g, response)
+    let usernames = search(/([A-Za-z0-9_-]+)\sof\sproject|-{2}\s([A-Za-z_]+)\s/g, response)
 
     return usernames
   }
